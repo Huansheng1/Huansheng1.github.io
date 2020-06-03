c087fb7daa62d2c096c2190e18a4be4e0d68b0aa
Huansheng1 <2933903535@qq.com>
2020-06-03 22:43:14 +0800

    11

---
+++ assets/js/load.js
@@ -0,0 +1,26 @@
+console.log('我运行了');
+
+(function() {
+    var bp = document.createElement('script');
+    var curProtocol = window.location.protocol.split(':')[0];
+    if (curProtocol === 'https') {
+        bp.src = 'https://zz.bdstatic.com/linksubmit/push.js';
+    } else {
+        bp.src = 'http://push.zhanzhang.baidu.com/push.js';
+    }
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(bp, s);
+
+    // 修改来源属性
+    var referrer = document.querySelector("meta[name=referrer]");
+    if (referrer) {
+        console.log(referrer.getAttribute("content"));
+        referrer.setAttribute('content', 'never');
+        console.log(document.querySelector("meta[name=referrer]").getAttribute("content"));
+    } else {
+        referrer = document.createElement('meta');
+        referrer.name = "referrer";
+        referrer.content = "never";
+        document.getElementsByTagName('head')[0].appendChild(referrer);
+    }
+})();
